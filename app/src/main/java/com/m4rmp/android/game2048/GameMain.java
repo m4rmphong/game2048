20eeb57a55dcd7617af48ab83b84fe6b795c1db5
Susan Hong <m4rmphong.cs01@g2.nctu.edu.tw>
2019-12-31 22:38:17 +0800

    Gameflow: Initialization & Reset
    1. Game data & UI initialization
    2. Reset button implementation
    3. Log

--- app/src/main/java/com/m4rmp/android/game2048/GameMain.java
+++ app/src/main/java/com/m4rmp/android/game2048/GameMain.java
@@ -3,18 +3,26 @@ package com.m4rmp.android.game2048;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameMain extends AppCompatActivity {
     private static final String TAG = "GameMain";
 
     // game manager
+    private boolean mWin = false;
     private int mScore = 0;
     private int mRecord = 0;
+    private Random mRand;
 
     // views
     private TextView mScoreText;
     private TextView mRecordText;
+    private Button mResetButton;
 
 
     @Override
@@ -25,14 +33,58 @@ public class GameMain extends AppCompatActivity {
         init();
     }
 
+    static void log(String type, String aMessage) {
+        if (type == "e") {
+            Log.e(TAG, aMessage);
+        } else if (type == "w") {
+            Log.w(TAG, aMessage);
+        } else if (type == "i") {
+            Log.i(TAG, aMessage);
+        } else if (type == "d") {
+            Log.d(TAG, aMessage);
+        } else if (type == "v") {
+            Log.v(TAG, aMessage);
+        }
+    }
+
     private void init() {
         // UI component
-        mScoreText = (TextView) findViewById(R.id.score);
+        mScoreText = findViewById(R.id.score);
+        mRecordText = findViewById(R.id.record);
+        mResetButton = findViewById(R.id.reset_button);
+
+        // game data
+        mRand = new Random();
+        initGame();
+        initUI();
+    }
+
+    private void initGame() {
+        // new game
+        resetGame();
+    }
+
+    private void initUI() {
         mScoreText.setText(Integer.toString(mScore));
-        mRecordText = (TextView) findViewById(R.id.record);
         mRecordText.setText(Integer.toString(mRecord));
 
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+                updateUi();
+            }
+        });
+    }
 
+    private void resetGame() {
+        mWin = false;
+        mScore = 0;
+        // TODO: resetGrid()
+    }
+
+    private void updateUi() {
+        mScoreText.setText(Integer.toString(mScore));
     }
 
 }
